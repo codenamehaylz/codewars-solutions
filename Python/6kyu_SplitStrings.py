31a9598c8cb94e8139b47f8b7895083281e26950
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-11-10 19:54:15 +0000

    Added my best solution plus my fav from others

--- Python/6kyu_SplitStrings.py
+++ Python/6kyu_SplitStrings.py
@@ -2,8 +2,29 @@
 # If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
 
 # Examples:
-# * 'abc' =>  ['ab', 'c_']
-# * 'abcdef' => ['ab', 'cd', 'ef']
+# 'abc' =>  ['ab', 'c_']
+# 'abcdef' => ['ab', 'cd', 'ef']
+
+
+#* My Solution!
 
 def solution(s):
-    pass
+    pairs = []
+    if len(s) % 2 != 0:
+        s += '_'
+    for i in range(0, len(s), 2):
+        pair = s[i] + s[i+1]
+        pairs.append(pair)
+    return pairs
+        
+print(solution('abcde'))
+
+
+#* My favourite solution from others!
+
+import re
+
+def other_solution(s):
+    return re.findall(".{2}", s + "_")
+
+print(other_solution('abcde'))
